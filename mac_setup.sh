554c8543ab295678f1c9b9407363b01e9ff5b9fe
itSpongee <65823967+itSpongee@users.noreply.github.com>
2022-09-04 11:16:49 -0500

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -5,13 +5,16 @@
 
 # install all applications and cli tools
 brew install python openjdk git node npm gcc wget &&
-brew install --cask steam minecraft firefox-developer-edition google-chrome-dev microsoft-edge discord skype zoom eqmac streamlabs-obs vlc adobe-creative-cloud blender autodesk-fusion360 visual-studio-code github unity vmware-fusion wireshark qbittorrent ti-connect-ce google-drive vitalsource-bookshelf clickup 
+brew install --cask steam minecraft homebrew/cask-versions/firefox-developer-edition google-chrome-dev microsoft-edge discord skype zoom eqmac streamlabs-obs vlc adobe-creative-cloud blender autodesk-fusion360 visual-studio-code github unity vmware-fusion wireshark qbittorrent ti-connect-ce google-drive vitalsource-bookshelf clickup 
+
+# startup Firefox to generate necessary directories
+open /Applications/Firefox\ Developer\ Edition
 
 # configure git
 git config --global user.name "spongee" && git config --global user.email "coding.guru16@gmail.com"
 
 # install & oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # configure oh-my-zsh
 curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/.zshrcMAC -o ~/.zshrc
@@ -22,13 +25,15 @@ git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${Z
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # install userChrome.css
-curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/userChrome.css -o ~/Library/Application\ Support/Firefox/Profiles/*.dev-edition-default/chrome/userChrome.css
+export FIREFOX_PROFILE="$(echo ~/Library/Application\ Support/Firefox/Profiles/*.dev-edition-default)" && \
+curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/userChrome.css -o userChrome.css && \
+mkdir $FIREFOX_PROFILE/chrome/ && mv userChrome.css $FIREFOX_PROFILE/chrome/
 
 # retrieve nightTab backup
 curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/nightTab.json -o ~/Desktop/nightTab.json
 
 # update shell & finish
-source ~/.zshrc && clear
+clear
 
 echo "The following applications and configurations must be installed manually:\n
     * XCode\n
@@ -37,3 +42,5 @@ echo "The following applications and configurations must be installed manually:\
     * BlueStacks\n
     * Microsoft OneNote\n
     * Firefox nightTab (.json file located in ~/Desktop)"
+
+source ~/.zshrc
