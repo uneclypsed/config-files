20b0119bb14cf9f4f5fe08222e0c31ac1de2eca9
itSpongee <65823967+itSpongee@users.noreply.github.com>
2022-09-04 11:29:44 -0500

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -8,32 +8,32 @@ brew install python openjdk git node npm gcc wget &&
 brew install --cask steam minecraft homebrew/cask-versions/firefox-developer-edition google-chrome-dev microsoft-edge discord skype zoom eqmac streamlabs-obs vlc adobe-creative-cloud blender autodesk-fusion360 visual-studio-code github unity vmware-fusion wireshark qbittorrent ti-connect-ce google-drive vitalsource-bookshelf clickup 
 
 # startup Firefox to generate necessary directories
-open /Applications/Firefox\ Developer\ Edition
+open /Applications/Firefox\ Developer\ Edition && \
 
 # configure git
-git config --global user.name "spongee" && git config --global user.email "coding.guru16@gmail.com"
+git config --global user.name "spongee" && git config --global user.email "coding.guru16@gmail.com" && \
 
 # install & oh-my-zsh
-zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
 
 # configure oh-my-zsh
-curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/.zshrcMAC -o ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/.zshrcMAC -o ~/.zshrc && \
 
 # install oh-my-zsh plugins
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting &&
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete &&
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting && \
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete && \
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
 
 # install userChrome.css
 export FIREFOX_PROFILE="$(echo ~/Library/Application\ Support/Firefox/Profiles/*.dev-edition-default)" && \
 curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/userChrome.css -o userChrome.css && \
-mkdir $FIREFOX_PROFILE/chrome/ && mv userChrome.css $FIREFOX_PROFILE/chrome/
+mkdir $FIREFOX_PROFILE/chrome/ && mv userChrome.css $FIREFOX_PROFILE/chrome/ && \
 
 # retrieve nightTab backup
-curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/nightTab.json -o ~/Desktop/nightTab.json
+curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/nightTab.json -o ~/Desktop/nightTab.json && \
 
 # update shell & finish
-clear
+clear && \
 
 echo "The following applications and configurations must be installed manually:\n
     * XCode\n
@@ -41,6 +41,6 @@ echo "The following applications and configurations must be installed manually:\
     * VEXCode V5 Pro\n
     * BlueStacks\n
     * Microsoft OneNote\n
-    * Firefox nightTab (.json file located in ~/Desktop)"
+    * Firefox nightTab (.json file located in ~/Desktop)" && \
 
 source ~/.zshrc
