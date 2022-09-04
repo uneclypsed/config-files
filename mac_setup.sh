9018123a91e92795454a93f8f13db51462e00619
itSpongee <65823967+itSpongee@users.noreply.github.com>
2022-09-04 17:44:26 -0500

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -14,8 +14,7 @@ open /Applications/Firefox\ Developer\ Edition.app
 git config --global user.name "spongee" && git config --global user.email "coding.guru16@gmail.com"
 
 # install oh-my-zsh
-set +e
-zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && \
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sed s/'set -e'/''/)" && \
 
 # configure oh-my-zsh
 curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/.zshrcMAC -o ~/.zshrc && \
