2e962227f6bf17303ef383f1e6bd39d4e5efc1c1
itSpongee <65823967+itSpongee@users.noreply.github.com>
2022-12-27 14:55:40 -0600

    added arch linux configs

---
+++ arch_setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# install git
+sudo pacman -Syu
+sudo pacman -S --noconfirm git
+
+# install yay
+sudo pacman -S --noconfirm base-devel
+cd /opt && git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $(whoami):$(whoami) ./yay-git
+cd yay-git
+makepkg -si
+
+# install oh-my-zsh
+RUNZSH='no' zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# configure oh-my-zsh
+curl -fsSL https://raw.githubusercontent.com/itSpongee/config-files/main/.zshrcARCH -o ~/.zshrc
+
+# ricing
+## deps
+yay -S nerd-fonts-jetbrains-mono nerd-fonts-victor-mono nerd-fonts-mplus ttf-jetbrains-mono ttf-font-awesome ttf-material-design-icons ttf-remixicon ttf-icomoon-feather
+yay -S xorg sddm i3-gaps qt5-graphicaleffects-git qt5-quickcontrols-git kitty polybar rofi rofi-power-menu rofi-emoji dunst light picom-jonaburg-git i3lock-color feh bluez bluez-utils blueberry pulseaudio-control pavucontrol mpd ncmpcpp cava ueberzug w3m starship-git lxappearance kava neofetch lolcal cowsay wisdom-mod bash-pipes cbonsai bpytop network-manager-applet blueman clipit
+## gtk theme
+git clone https://github.com/stronk-dev/Tokyo-Night-Linux.git
+cd Tokyo-Night-Linux
+cp -a .config .xinitrc .Xresources -t ~
+cp -a usr/share/themes/TokyoNight /usr/share/themes/
+## sddm
+sudo systemctl enable sddm
+git clone https://github.com/rototrash/tokyo-night-sddm.git ~/tokyo-night-sddm
+sudo mv ~/tokyo-night-sddm /usr/share/sddm/themes/
+echo `sudo nano /etc/sddm.conf\n---\n[Theme]\nCurrent=tokyo-night-sddm` > /etc/sddm.conf
+## tokyo-night rice
+sudo systemctl enable --now bluetooth
+git clone https://github.com/rototrash/dotfiles.git
+cd dotfiles/home && git checkout tokyo-night
+cp -a .Xresources .gtkrc-2.0 .imwheelrc .scripts .local .config -t ~
+
+# install oh-my-zsh plugins
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting && \
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete && \
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
